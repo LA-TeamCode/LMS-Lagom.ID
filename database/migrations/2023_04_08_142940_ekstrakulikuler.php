47cf3378a468988d2df36b8d7120d60eb2c58e16
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 <alulgans70@gmail.com>
2023-04-09 07:33:20 +0700

    [FIX] Mengganti struktur tabel ekstrakulikuler: 	modified:   database/migrations/2023_04_08_142940_ekstrakulikuler.php

--- database/migrations/2023_04_08_142940_ekstrakulikuler.php
+++ database/migrations/2023_04_08_142940_ekstrakulikuler.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('ekstrakulikuler', function (Blueprint $table) {
             $table->id();
+            //remove
             $table->string('ekstrakulikuler');
         });
     }
